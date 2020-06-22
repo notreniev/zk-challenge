ae1e4d9ca243ff81374c62b17d8fa1b78040e192
notreniev <everton.canez@gmail.com>
2020-06-22 18:44:37 -0300

    Slot's scroll ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -46,8 +46,7 @@ export class HomeComponent implements OnInit {
       { time: '14:00' }, { time: '14:30' }, { time: '15:00' }, { time: '15:30' },
       { time: '16:00' }, { time: '16:30' }, { time: '17:00' }, { time: '17:30' },
       { time: '18:00' }, { time: '18:30' }, { time: '19:00' }, { time: '19:30' },
-      { time: '20:00' }, { time: '20:30' }, { time: '21:00' }, { time: '21:30' },
-      { time: 'MORE' }
+      { time: '20:00' }, { time: '20:30' }, { time: '21:00' }, { time: '21:30' }
     ]
   };
 
