149ae85ac5070fe692550fe3038265a43f3da732
everton <everton@Evertons-MacBook-Air.local>
2020-06-24 09:43:46 -0300

    Final touches

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
 ];
 
